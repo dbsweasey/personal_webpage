9b815558e3b30b99bc9a4f93a41eae212c77e107
dbsweasey <davidbsweasey@gmail.com>
2025-12-18 17:45:06 -0500

    fixing PROD backend URI issues

--- client/src/components/Spotify.jsx
+++ client/src/components/Spotify.jsx
@@ -6,8 +6,8 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const PORT = import.meta.env.VITE_API_PORT || "none";
 
 let url = BASE_URL;
-if (PORT === "none") {
-  console.log("In PROD, no port needed");
+if (PORT !== "none") {
+  console.log("In DEV, attaching port");
   url += `:${PORT}`;
 }
 
